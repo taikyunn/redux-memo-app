f894cee5f1a04a6ed912503562f95853683ba5a7
taikyunn <taichi_141@me.com>
2023-01-24 17:10:36 +0900

    memoアプリの作成

---
+++ frontend/src/memo/Store.js
@@ -0,0 +1,104 @@
+import { createStore } from 'redux';
+
+const initData = {
+  data: [{
+    message: 'sample data',
+    created: new Date()
+  }],
+  message: 'Please type message',
+  mode: 'default',
+  fdata: []
+}
+
+// レデューサー
+export function memoReducer(state = initData, action) {
+  switch (action.type) {
+    case 'ADD':
+      return addReduce(state, action);
+
+    case 'DELETE':
+      return deleteReduce(state, action);
+
+    case 'FIND':
+      return findReduce(state, action);
+
+    default:
+      return state;
+  }
+}
+
+// メモ追加のレデュース処理
+function addReduce(state, action) {
+  let data = {
+    message: action.message,
+    created: new Date()
+  };
+
+  let newData = state.data.slice();
+  newData.unshift(data);
+
+  return {
+    data: newData,
+    message: 'Added!',
+    mode: 'default',
+    fdata: []
+  };
+}
+
+// メモ検索のレデュース処理
+function findReduce(state, action) {
+  let f = action.find;
+  let fdata = [];
+  state.data.forEach((value) => {
+    if (value.message.indexOf(f) >= 0) {
+      fdata.push(value);
+    }
+  });
+
+  return {
+    data: state.data,
+    message: 'find:' + f,
+    mode: 'find',
+    fdata: fdata
+  };
+}
+
+// メモ削除のレデュース処理
+function deleteReduce(state,action) {
+  let newData = state.data.slice();
+  newData.splice(action.index, 1);
+  return {
+    data: newData,
+    message: 'delete' + action.index + ':',
+    mode: 'delete',
+    fdata: [],
+  }
+}
+
+// アクションクリエーター
+// メモ追加のアクション
+export function addMemo(text) {
+  return {
+    type: 'ADD',
+    message: text
+  }
+}
+
+// メモ削除のアクション
+export function deleteMemo(num) {
+  return {
+    type: 'DELETE',
+    index: num
+  }
+}
+
+// メモ検索のアクション
+export function findMemo(text) {
+  return {
+    type: 'FIND',
+    find: text
+  }
+}
+
+// ストアを作成
+export default createStore(memoReducer);
