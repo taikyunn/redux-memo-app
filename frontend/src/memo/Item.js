74ed79c9d11cccf9446ce00de8057059a020a224
taikyunn <taichi_141@me.com>
2023-01-27 10:37:05 +0900

    永続化。createdのデータがlocalstorageで変換されてうまく表示できなかった。

--- frontend/src/memo/Item.js
+++ frontend/src/memo/Item.js
@@ -30,12 +30,13 @@ class Item extends Component {
 
   render() {
     let d = this.props.value.created;
-    let f = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+    console.log(d);
+    // let f = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
     return (
       <tr>
         <th style={this.th}>No. {this.props.index}</th>
         <td style={this.td}>{this.props.value.message}</td>
-        <td style={this.date}>{f}</td>
+        <td style={this.date}>{}</td>
       </tr>
     );
   }
